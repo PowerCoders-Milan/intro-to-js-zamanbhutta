56349cf6893d6d0ea7df7e652a7aec8af647ebd0
zamanbhutta <marham.ayat@gmail.com>
2021-02-25 13:12:31 +0100

    ex3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,12 @@
 // Example
-window.alert("Hello World!");
+//window.alert("Hello World!");
+var num1= 10;
+var num2= 20;
+
+if(num1 > num2) {
+    console.log("The greater number of " + num1 + ' and ' + num2 + ' is ' + num1 + " . ");
+} else if (num2 > num1){
+    console.log("The greater number of " + num1 + ' and ' + num2 + ' is ' + num2 + ' . ')
+} else {
+    console.log ("the numbers are the same")
+}
