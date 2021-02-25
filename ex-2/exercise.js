04b2a87dfeb52f2f40e8a5b7e875de03c81f2800
zamanbhutta <marham.ayat@gmail.com>
2021-02-25 12:37:31 +0100

    ex2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,12 @@
 // Example
-window.alert("Hello World!");
+//window.alert("Hello World!");
+var BirthYear = prompt("Write your birth year");
+var CurrentYear = prompt("Write the current year");
+
+var age =  CurrentYear - BirthYear ;
+var age2 = age -1 ;
+
+window.alert("I will be either " + age2 +" or " + age + " in " +  CurrentYear + ".");
+
+console.log(age);
+
