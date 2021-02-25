47593f18983cd649aab4c6216d3f0adbe91c512b
zamanbhutta <marham.ayat@gmail.com>
2021-02-25 16:46:56 +0100

    exe6

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,2 +1,5 @@
 // Example
-window.alert("Hello World!");
+//window.alert("Hello World!");
+var name = prompt("write the quotation");
+var person= prompt("what the author name");
+window.alert(person + "  " + " says " + name +" . " );
