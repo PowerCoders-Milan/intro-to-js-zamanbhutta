214c14fdf2f43decec8e4ce3087be83446e4a865
zamanbhutta <marham.ayat@gmail.com>
2021-02-25 11:43:34 +0100

    ex1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,12 @@
 // Example
-window.alert("Hello World!");
+//window.alert("Hello World!");
+var numKids  = 2;
+var partner  = 'Bisma Zaman';
+var geoLocation = 'Milano';
+var jobTitle = 'web developer';
+
+var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ', and married to ' +
+   partner + ' ' + ' with ' + numKids + ' kids.';
+console.log(future);
+window.alert(future);
+
