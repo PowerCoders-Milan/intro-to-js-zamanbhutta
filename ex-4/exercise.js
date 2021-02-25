478ebb816a791c9929718588cc637760f31b43d4
zamanbhutta <marham.ayat@gmail.com>
2021-02-25 13:30:27 +0100

    ex4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,15 @@
 // Example
-window.alert("Hello World!");
+//window.alert("Hello World!");
+var x = prompt("Wirte your name please");
+window.alert("Hello " + x);
+window.alert("how your day was, Was it good or bad?");
+var y = prompt("write your answer");
+if (y == "good") {
+    window.alert("I'm happy for you champ");  
+}else if (y == "bad") {
+    window.alert("That's too bad, tomorrow it's a new day");
+}
+
+else {
+    window.alert("I do not recognize the answer");
+}
